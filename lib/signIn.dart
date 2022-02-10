57600eddb67b60ef4caa98a679b7e08eb1a8dac8
Azad Linavay <azadlinavay@hotmail.com>
2022-02-10 17:58:44 +0300

    updaTE

--- lib/signIn.dart
+++ lib/signIn.dart
@@ -1,14 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
 
-  Future<void> _signInAnonymously() async {
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  _signInAnonymously() async {
     final auth = FirebaseAuth.instance;
     final user = await auth.signInAnonymously();
     print('User signed in anonymously: $user');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _signInAnonymously();
   }
 
   @override
@@ -16,20 +29,55 @@ class SignInPage extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Sign In'),
+          title: Text('Welcome To Token Screen'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(
-                child: Text('Sign In Anonymously'),
-                onPressed: _signInAnonymously ,
-              ),
+              Text("AZAD"),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+Future<void> saveTokenToDatabase(String token) async {
+  // Assume user is logged in for this example
+  
+  String userId = FirebaseAuth.instance.currentUser!.uid;
+
+  await FirebaseFirestore.instance.collection('users').doc(userId).update({
+    'tokens': FieldValue.arrayUnion([token]),
+  });
+}
+
+class Application extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _Application();
+}
+
+class _Application extends State<Application> {
+  String? _token;
+
+  @override
+  void initState() async {
+    super.initState();
+    // Get the token each time the application loads
+    String? token = await FirebaseMessaging.instance.getToken();
+    debugPrint('Token: $token');
+
+    // Save the initial token to the database
+    await saveTokenToDatabase(token ?? '');
+
+    // Any time the token refreshes, store this in the database too.
+    FirebaseMessaging.instance.onTokenRefresh.listen(saveTokenToDatabase);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(_token ?? '');
   }
 }
