32f8f4b186ce572813d7b5c6c328f8e2d23d0559
Azad Linavay <azadlinavay@hotmail.com>
2022-02-11 18:40:35 +0300

    Edited Push Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:push_notification/signIn.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp().whenComplete(() {
     print("Firebase initialized");
   });
-  runApp(SignInPage());
+
+  await FirebaseAuth.instance.signInAnonymously().then((UserCredential) async {
+    await FirebaseMessaging.instance.getToken().then((token) async {
+      print("Token: $token");
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(UserCredential.user!.uid)
+          .set({
+        "token": token,
+        "uid": UserCredential.user!.uid,
+      });
+    });
+  });
+
+  runApp(const SignInPage());
 }
