4e855a7396a567c3868a31b7c4416f1d08abd3ea
Azad Linavay <azadlinavay@hotmail.com>
2022-02-11 18:43:22 +0300

    Created Get Token Class in another page

---
+++ lib/getTokenClass.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> GetToken() async {
+  await FirebaseAuth.instance.signInAnonymously().then((UserCredential) async {
+    await FirebaseMessaging.instance.getToken().then((token) async {
+      print("Token: $token");
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(UserCredential.user!.uid)
+          .set({
+        "token": token,
+        "uid": UserCredential.user!.uid,
+      });
+    });
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:push_notification/getTokenClass.dart';
 import 'package:push_notification/signIn.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
@@ -11,18 +12,6 @@ void main() async {
     print("Firebase initialized");
   });
 
-  await FirebaseAuth.instance.signInAnonymously().then((UserCredential) async {
-    await FirebaseMessaging.instance.getToken().then((token) async {
-      print("Token: $token");
-      await FirebaseFirestore.instance
-          .collection("users")
-          .doc(UserCredential.user!.uid)
-          .set({
-        "token": token,
-        "uid": UserCredential.user!.uid,
-      });
-    });
-  });
-
+  await GetToken();
   runApp(const SignInPage());
 }
